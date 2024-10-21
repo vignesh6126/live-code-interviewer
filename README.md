59e407abb45fdda3cab60353291a52718ea69575
Antonio Oliveira <antoniovini47@gmail.com>
2024-10-21 16:00:33 -0300

    First funcional version using Video and Code Editor simultaneosly.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# live-code-interviewer
+A live code interview tool powered by monaco, using SuperViz SDK.
+
 # React + TypeScript + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,118 @@
-import { useState } from "react";
+import { VideoConference } from "@superviz/react-sdk";
 import "./App.css";
+import { SuperVizRoomProvider, useYjsProvider, YjsProvider } from "@superviz/react-sdk";
+import { sampleInfo } from "./projectInfo";
+import * as Y from "yjs";
+import { Editor } from "@monaco-editor/react";
+import { useEffect, useState } from "react";
+import { MonacoBinding } from "y-monaco";
 
-function App() {
+const DEVELOPER_API_KEY = import.meta.env.VITE_SUPERVIZ_DEVELOPER_KEY;
+
+const groupId = sampleInfo.id;
+const groupName = sampleInfo.name;
+// Sample participant ID (0-100)
+// const participant = Math.floor(Math.random() * 100)
+//   .toString()
+//   .padStart(3, "0");
+
+const ydoc = new Y.Doc();
+
+const style = document.createElement("style");
+style.id = "sv-yjs-monaco";
+document.head.appendChild(style);
+
+function EditorRoom() {
+  const { provider } = useYjsProvider();
+  const [editor, setEditor] = useState<any>(null);
+
+  useEffect(() => {
+    if (!provider || editor == null) return;
+
+    const binding = new MonacoBinding(
+      ydoc.getText("monaco"),
+      editor.getModel()!,
+      new Set([editor]),
+      provider.awareness
+    );
+
+    return () => {
+      binding.destroy();
+    };
+  }, [ydoc, provider, editor]);
+
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <YjsProvider doc={ydoc} />
+      <Editor
+        defaultValue="// Connect to the room to start collaborating"
+        defaultLanguage="typescript"
+        onMount={(editor) => {
+          setEditor(editor);
+        }}
+        options={{
+          padding: {
+            top: 32,
+          },
+        }}
+        theme="vs-dark"
+      />
+    </>
+  );
+}
+
+function VideoRoom() {
+  const collaborationMode = {
+    enabled: true,
+  };
+  return <VideoConference participantType="host" collaborationMode={collaborationMode} />;
+}
+
+function App() {
+  const [userID, setUserID] = useState<string | null>(null);
+
+  return (
+    <>
+      <input type="text" placeholder="userID" />
+      <button
+        onClick={() => {
+          const input = document.querySelector('input[type="text"]');
+          if (input) {
+            setUserID(input.value);
+          }
+        }}>
+        Iniciar
+      </button>
+      {userID ? (
+        <SuperVizRoomProvider
+          developerKey={DEVELOPER_API_KEY}
+          group={{
+            id: groupId,
+            name: groupName,
+          }}
+          participant={{
+            id: userID,
+            name: "userName",
+          }}
+          roomId="ROOM_ID">
+          <EditorRoom />
+        </SuperVizRoomProvider>
+      ) : null}
+      {userID ? (
+        <SuperVizRoomProvider
+          developerKey={DEVELOPER_API_KEY}
+          group={{
+            id: groupId,
+            name: groupName,
+          }}
+          participant={{
+            id: userID.toString() + "video",
+            name: "userName",
+          }}
+          roomId="ROOM_ID">
+          <VideoRoom />
+        </SuperVizRoomProvider>
+      ) : null}
     </>
   );
 }
