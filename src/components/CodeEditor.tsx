2739b5f267abab622b778dc8688ddf9760cb43c8
Antonio Oliveira <antoniovini47@gmail.com>
2024-10-24 21:42:42 -0300

    Reverse git correction

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -8,7 +8,11 @@ import { YjsProvider, useYjsProvider } from "@superviz/react-sdk";
 import * as Y from "yjs";
 import { MonacoBinding } from "y-monaco";
 import * as monaco from "monaco-editor";
-import postCode from "../services/postCode";
+// import postCode from "../services/postCode";
+// import { getFirestore } from "firebase/firestore";
+// import { initializeApp } from "firebase/app";
+// initializeApp();
+// const dbFirestore = getFirestore();
 
 const ydoc = new Y.Doc();
 
@@ -17,6 +21,9 @@ const CodeEditor = (props: { roomId: string }) => {
   const [value, setValue] = useState<string>("//Code goes here");
   const [language, setLanguage] = useState("javascript");
   const { provider } = useYjsProvider();
+
+  // test TODO: remove
+  props.roomId = "test";
 
   const onMount = (editor: monaco.editor.IStandaloneCodeEditor) => {
     editorRef.current = editor;
@@ -45,18 +52,21 @@ const CodeEditor = (props: { roomId: string }) => {
     };
   }, [provider]);
 
+  // async function saveCode() {
+  //   const docRef = dbFirestore.collection("codes").doc(props.roomId);
+
+  //   await docRef.set({
+  //     code: value,
+  //   });
+  // }
+
   return (
     <YjsProvider doc={ydoc}>
       <Box>
         <HStack spacing={4}>
           <Box w="50%">
             <LanguageSelector language={language} onSelect={onSelect} />
-            <button
-              onClick={() => {
-                postCode(props.roomId, value);
-              }}>
-              Save this code
-            </button>
+            {/* <button onClick={saveCode}>Save this code</button> */}
             <Editor
               options={{
                 minimap: {
